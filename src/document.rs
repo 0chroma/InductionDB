426ec4169afe1a2a6fb26c62b3d2ad5b0dd19d0c
0chroma <21014+0chroma@users.noreply.github.com>
2024-01-14 04:12:59 -0800

    more work on document sync + tests

--- src/document.rs
+++ src/document.rs
@@ -3,15 +3,18 @@ use yrs::Doc;
 
 pub struct Document {
     id: Uuid,
-    yrs_doc: Doc,
+    crdt: Doc,
 }
 
 impl Document {
-    pub fn new() -> () {
+    pub fn new() -> Document {
         Document {
             id: Uuid::new_v4(),
-            yrs_doc: Doc::new(),
-        };
+            crdt: Doc::new(), //todo make this into a trait so we can swap between crdts
+        }
+    }
+    pub fn sync(&self, doc: &Document) -> &Document {
+        self
     }
     pub fn serialize(&self) -> () {}
     pub fn deserialize(&self) -> () {}
--- tests/document.rs
+++ tests/document.rs
@@ -5,4 +5,9 @@ use inductiondb::*;
 fn sync_two_documents() {
     let doc1 = Document::new();
     let doc2 = Document::new();
+    //todo make change to doc2
+    let synced_doc = Document::sync(&doc1, &doc2);
+    assert_eq!(Document::serialize(&synced_doc), Document::serialize(&doc2));
 }
+
+//todo test serialization
